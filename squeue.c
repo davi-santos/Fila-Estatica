ec82dec63385a26afa17fd8f0d738cbb15295d20
Davi Santos <davixd14@gmail.com>
2017-05-02 12:48:17 -0300

    Adicionando vida()

--- squeue.c
+++ squeue.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #define N 10
 
-
 	SQueue* SQueue_create (){
 		SQueue* novo = (SQueue*) malloc (sizeof(SQueue));
 
@@ -15,8 +14,8 @@
 
 	int SQueue_insert (SQueue* F, int dado){
 
-		if((F -> in + 1)%N != F -> out){
-			F -> in = (F -> in +1)%N;
+		if((F -> in + 1) % N != F -> out){
+			F -> in = (F -> in + 1)%N;
 			F -> dados [F -> in] = dado;
 			F -> out = (F -> out == -1)? 0: F -> out;
 			return 1;
@@ -29,7 +28,7 @@
 	int SQueue_remove(SQueue* F, int* dado){
 	
 		if (F -> out >= 0){
-			*dado = F -> dados [F->out];
+			*dado = F -> dados [F -> out];
 			F -> out = (F -> out + 1) % N;
 			
 			if(F -> out > F-> in){
@@ -43,6 +42,12 @@
 	}
 
 	void SQueue_print (SQueue* F){
+		int i;
+		for(i = F -> out; i != F -> in; i = (i+1) % N)
+			printf("%d ", F -> dados[i]);
+	}
+
+	void SQueue_print2 (SQueue* F){
 		int i;
 		for(i = F -> out; i != F -> in; i = (i+1) % N)
 			printf("%d ", F -> dados[i]);
